49cfa66009046dcf03dea8bc9dd8c8354aeb3f57
devaoun <devaoun147@gmail.com>
2025-07-31 17:44:18 +0700

    .

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: '/relearn_test/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
